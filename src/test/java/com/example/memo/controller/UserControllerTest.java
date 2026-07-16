a7b0639a92f0e77004f6158337c1ede435de6c60
kampa <francescocampagnaro1994@gmail.com>
2026-07-16 11:06:55 +0200

    feat: add updateUser test

--- src/test/java/com/example/memo/controller/UserControllerTest.java
+++ src/test/java/com/example/memo/controller/UserControllerTest.java
@@ -40,16 +40,26 @@ public class UserControllerTest {
     @Test
     void shouldGetUser() {
         when(userService.getById(1L)).thenReturn(responseDto);
-        UserResponseDto risultato = userController.getById(1L);
-        assertThat(risultato.getId()).isEqualTo(1L);
-        assertThat(risultato.getNome()).isEqualTo("Francesco");
+        UserResponseDto result = userController.getById(1L);
+        assertThat(result.getId()).isEqualTo(1L);
+        assertThat(result.getNome()).isEqualTo("Francesco");
     }
     @Test
     void shouldGetAllUsers() {
         when(userService.getAll()).thenReturn(List.of(responseDto));
-        List<UserResponseDto> risultato = userController.getAll();
-        assertThat(risultato).hasSize(1);
-        assertThat(risultato.get(0).getId()).isEqualTo(1L);
-        assertThat(risultato.get(0).getNome()).isEqualTo("Francesco");
+        List<UserResponseDto> result = userController.getAll();
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0).getId()).isEqualTo(1L);
+        assertThat(result.get(0).getNome()).isEqualTo("Francesco");
+    }
+
+    @Test
+    void shouldUpdateUser() {
+        var newRequestDto = new UserRequestDto("Francesco2", "francesco@test.com", "newpassword123");
+        var newResponseDto = new UserResponseDto(1L, "Francesco2", "francesco@test.com");
+        when(userService.update( newRequestDto)).thenReturn(newResponseDto);
+        var result = userController.update(newRequestDto);
+        assertThat(result.getId()).isEqualTo(1L);
+        assertThat(result.getNome()).isEqualTo("Francesco2");
     }
 }
